782611a979c94e1781c1f479dc178dbcfc10e092
yepengding <dingyepeng@gmail.com>
2023-04-12 23:17:30 +0900

    Update deployment script.

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,23 +1,20 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  // Get deployer
+  const [deployer] = await ethers.getSigners();
 
-  const lockedAmount = ethers.utils.parseEther("0.001");
+  // Deploy
+  const Solidify = await ethers.getContractFactory("Solidify");
+  const solidify = await Solidify.deploy();
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
+  await solidify.deployed();
 
   console.log(
-    `Lock with ${ethers.utils.formatEther(lockedAmount)}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+    `Solidify is deployed to ${solidify.address} by account ${deployer.address}`
   );
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
